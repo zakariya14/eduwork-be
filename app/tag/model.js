31941e3696584ed05dbc250c0df54e7ddba59b91
edi-hartono <edi.eduwork@gmail.com>
2021-10-11 10:34:30 +0700

    no message

---
+++ app/tag/model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { model, Schema } = mongoose;
+const tagSchema = Schema({
+   name: {
+     type: String, 
+     minlength: [3, 'Panjang nama kategori minimal 3 karakter'], 
+     maxLength: [20, 'Panjang nama kategori maksimal 20 karakter'],
+     required: [true, 'Nama kategori harus diisi']
+   }
+});
+
+module.exports = model('Tag', tagSchema)
